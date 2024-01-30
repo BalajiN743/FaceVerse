79f19ad644bdbea5e6cb919da62458e678a442e0
Balaji_Nagarajan <118147240+BalajiN743@users.noreply.github.com>
2024-01-30 07:54:00 +0530

    Update __init__.py

--- faceversev3_jittor/faceverse_cuda/__init__.py
+++ faceversev3_jittor/faceverse_cuda/__init__.py
@@ -2,7 +2,7 @@ from faceverse_cuda.FaceVerseModel import FaceVerseModel
 import numpy as np
 
 def get_faceverse(**kargs):
-    model_path = 'data/faceverse_v3_6_s.npy'
+    
     faceverse_dict = np.load(model_path, allow_pickle=True).item()
     #for i in range(52):
     #    print(i, faceverse_dict['exp_name_list_52'][i])
